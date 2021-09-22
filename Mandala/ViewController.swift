54b9535e83f81b9cd64772fc2c093cfece923183
Maga <maga.rajic2604@gmail.com>
2021-09-22 03:02:07 -0700

    Initial Commit

---
+++ Mandala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mandala
+//
+//  Created by Maga Rajic on 22.09.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
